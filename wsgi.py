65b0ab27565c4dfc5ed2ba832a3644074859cca6
Shifu Nagibulla <85980955+nashdejovial@users.noreply.github.com>
2025-06-20 13:36:39 +0100

    Fix import paths

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
-from __init__ import create_app
+import os
+import sys
 
-app = create_app()
+# Add the application directory to the Python path
+app_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, app_dir)
+
+from app import app
 
 if __name__ == "__main__":
     app.run() 
